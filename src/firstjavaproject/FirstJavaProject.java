f898217fc110700d8efc81843ef27b2efa3149e2
Godwin Ofwono <godwinofwono933@gmail.com>
2025-03-20 14:34:38 +0300

    First Java Code for me

---
+++ src/firstjavaproject/FirstJavaProject.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package firstjavaproject;
+
+/**
+ *
+ * @author godwin-ofwono
+ */
+public class FirstJavaProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Java Devs");
+        System.out.println("Am glad to be coding Java Today");
+    }
+    
+}
