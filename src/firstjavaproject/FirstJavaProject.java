cf42e8117b5e53ca27fc413122a3c6f42749669c
Godwin Ofwono <godwinofwono933@gmail.com>
2025-03-20 15:34:51 +0300

    learnt to  deal  with String ,int variables as well as concantenating++ them

--- src/firstjavaproject/FirstJavaProject.java
+++ src/firstjavaproject/FirstJavaProject.java
@@ -14,8 +14,25 @@ public class FirstJavaProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // The code below just prints some few words
         System.out.println("Hello Java Devs");
-        System.out.println("Am glad to be coding Java Today");
+        System.out.println("Am glad to be coding Java Today. Welcome All beginers");
+        //Playing around with variables
+        String name = "Ofwono";
+        int myage = 22;
+        //here lets Concantenate the variables. w use +
+        System.out.println("My name is "+name+" and am "+myage+" years Old");
+        
+        //Copying Variable Values
+        int herAge = myage;
+        System.out.println("She is " +herAge+" years old");
+        
+        int temp = 40;
+        System.out.println("The Temperature in Sudan is "+temp);
+        
+        int egyptTemp = temp;
+        System.out.println("The Temperatur in Egypt is "+egyptTemp+" Degrees Cell");
+                
     }
     
 }
